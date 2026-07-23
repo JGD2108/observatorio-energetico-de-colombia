3ea4ef1845f6900f8f3167f604ba71f27e23bcbd
JGD2108 <gomezdelahoz@uninorte.edu.co>
2026-07-23 14:48:29 -0500

    Rename notebook to Windows-compatible filename

--- observatorio-energetico-astro/observatorio-energetico-astro/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  output: 'static',
-  site: 'https://observatorio-energetico.vercel.app',
-  build: {
-    assets: 'assets'
-  }
-});
